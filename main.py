b37e73402e82b8850790a23704444cbac479429e
Lesha <qu4rtz43@gmail.com>
2024-03-13 17:53:13 +0300

    add "Список профессий"

--- main.py
+++ main.py
@@ -10,5 +10,18 @@ def index(prof):
     return render_template('trainings.html', **param)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+    param = {}
+    param['list'] = list
+    param["professions"] = professions
+    return render_template('professions.html', **param)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
